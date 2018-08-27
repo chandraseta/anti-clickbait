6394f31754451005f902c50ccdebe88baed81a15
Rionaldi Chandraseta <rionaldi.chandraseta@gmail.com>
2018-08-27 14:54:37 +0700

    Combined all links to one file

--- src/crawler.py
+++ src/crawler.py
@@ -14,13 +14,10 @@ for cat in KOMPAS_CATEGORIES:
     data = request.text
     soup = BeautifulSoup(data)
 
-    # soupfile = open('../data/{}-kompas-soup.txt'.format(cat), '+')
-    # soupfile.write(str(soup))
-
     # links = soup.find_all('a')
     links = soup.find_all('a', class_=['headline__big__link', 'headline__thumb__link', 'article__link', 'most__link'])
 
-    linkfile = open('../data/{}-kompas-links.txt'.format(cat), 'a+')
+    linkfile = open('../data/kompas-links.txt'.format(cat), 'a+')
 
     for link in links:
         linkfile.write(link.get('href') + '\n')
