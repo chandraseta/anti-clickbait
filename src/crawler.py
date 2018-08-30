47acdab635e6f30222562b44e721a4d6209c00d4
Rionaldi Chandraseta <rionaldi.chandraseta@gmail.com>
2018-08-30 11:45:21 +0700

    Add back 'properti' for KOMPAS_CATEGORIES, rerun crawler

--- src/crawler.py
+++ src/crawler.py
@@ -4,15 +4,14 @@ from bs4 import BeautifulSoup
 
 # Crawler for kompas.com
 
-KOMPAS_CATEGORIES = ['news', 'ekonomi', 'bola', 'tekno', 'sains', 'entertainment', 'otomotif', 'lifestyle', 'travel', 'edukasi']
-# KOMPAS_CATEGORIES = ['news']
+KOMPAS_CATEGORIES = ['news', 'ekonomi', 'bola', 'tekno', 'sains', 'entertainment', 'otomotif', 'lifestyle', 'properti', 'travel', 'edukasi']
 
 for cat in KOMPAS_CATEGORIES:
     url = 'https://' + cat + '.kompas.com'
     request = requests.get(url)
 
     data = request.text
-    soup = BeautifulSoup(data)
+    soup = BeautifulSoup(data, 'html.parser')
 
     # links = soup.find_all('a')
     links = soup.find_all('a', class_=['headline__big__link', 'headline__thumb__link', 'article__link', 'most__link'])
