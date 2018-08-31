c7e67a01253187308f60392c615a4b2bd00bb09c
Rionaldi Chandraseta <rionaldi.chandraseta@gmail.com>
2018-09-01 01:07:07 +0700

    Add raw data processor to separate from raw data to title and content

---
+++ src/processor.py
@@ -0,0 +1,30 @@
+# Processor for kompas-raw.txt
+
+rawfile = open('../data/kompas-raw.txt', 'r')
+
+raw_data = []
+
+for idx, line in enumerate(rawfile):
+    if idx % 2 == 1:
+        line = line[3:]
+        raw_data.append(line)
+
+titlefile = open('../data/kompas-data-titles.txt', 'a+')
+contentfile = open('../data/kompas-data-contents.txt', 'a+')
+
+for data in raw_data:
+    title = data.split('|')[0]
+    data = data.replace(title, '')
+
+    data = data[2:]
+
+    if '- Kompas.com' in title:
+        title = title.replace('- Kompas.com', '')
+        title = title.strip(' ')
+
+    # Remove duplicate titles in front of the articles
+    data = data.replace(title, '')
+    data = data.strip(' ')
+
+    titlefile.write(title + '\n')
+    contentfile.write(data)
