4d65e77e01151bb06ea233386fda2ea72c2d8340
Rionaldi Chandraseta <rionaldi.chandraseta@gmail.com>
2018-10-29 19:51:17 +0700

    Modify content-processor and add updated data

--- src/content-processor.py
+++ src/content-processor.py
@@ -1,4 +1,5 @@
 # Processor for kompas-data-contents.txt
+MIN_IDX = 100
 
 contentfile = open('../data/kompas-data-contents.txt', 'r')
 
@@ -10,26 +11,28 @@ for line in contentfile:
 processed_contentfile = open('../data/kompas-data-contents-processed.txt', 'w+')
 
 for id, content in enumerate(content_data):
-    idxs = [0, 0, 0, 0]
-    idxs[0] = content.find('-')
-    idxs[1] = content.find('–')
-    idxs[2] = content.find('—')
-    idxs[3] = content.find('--')
+    # idxs = [0, 0, 0, 0]
+    idx = content.find('KOMPAS.com')
+    # idxs[1] = content.find('–')
+    # idxs[2] = content.find('—')
+    # idxs[3] = content.find('--')
 
     # If string is not found, add 100 to make the idx big
-    for i, idx in enumerate(idxs):
-        if idx < 0:
-            idxs[i] += 100
+    # for i, idx in enumerate(idxs):
+    #     if idx < 0:
+    #         idxs[i] += MIN_IDX
 
-    separator_idx = min(idxs)
-    d_dash = separator_idx == idxs[3]
+    # separator_idx = min(idxs)
+    # d_dash = separator_idx == idxs[3]
     
-    start_index = separator_idx + 1
-    if d_dash:
-        start_index += 1
+    # start_index = separator_idx + 1
+    # if d_dash:
+        # start_index += 1
 
-    if start_index <= 100:
-        content = content[start_index:]
-        content = content.strip()
-
+    # if start_index <= MIN_IDX:
+        # content = content[start_index:]
+    content = content[idx + 12:]
+    content = content.strip('-')
+    content = content.strip()
+        
     processed_contentfile.write(content + '\n')
