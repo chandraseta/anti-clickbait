66bc9387930c6e13902e91dbd7c20e5df1934a63
Rionaldi Chandraseta <rionaldi.chandraseta@gmail.com>
2018-10-29 20:43:08 +0700

    Add contents dataset

---
+++ src/content-dataset-builder.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+contentfile = open('../data/kompas-data-contents-processed.txt', 'r')
+contents = []
+
+for line in contentfile:
+    line = line.strip()
+    if line == '':
+        line = 'None'
+    contents.append(line)
+
+dataframe = pd.DataFrame({'contents': contents})
+dataframe.to_csv('../data/kompas-contents.csv', encoding='utf-8', sep='|', index= False, quoting=3, escapechar='\\')
